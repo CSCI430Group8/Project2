2a8a84fea12606c8cfc7831d18e65d72a54f4754
DuctTape119 <55901547+DuctTape119@users.noreply.github.com>
2020-04-02 11:59:07 -0500

    Updated WarehouseState, now it goes to the OpeningState menu

--- WarehouseState.java
+++ WarehouseState.java
@@ -1,7 +1,7 @@
 public abstract class WarehouseState {
 	protected static Context context;
 	protected WarehouseState() {
-		context = Context.instance();
+		//context = Context.instance();
 	}
 	public abstract void run();
 }
